123b28b859ae579fd3c85419bf97c18e4185d0ba
mohamedderouiche <mohamedamine.derouiche@esprit.tn>
2024-07-31 11:42:00 +0100

    update

--- src/app/component/courses/courses/courses.component.ts
+++ src/app/component/courses/courses/courses.component.ts
@@ -1,4 +1,49 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { CourseServiceService } from 'src/Services/course-service.service';
+
+// @Component({
+//   selector: 'app-courses',
+//   templateUrl: './courses.component.html',
+//   styleUrls: ['./courses.component.scss']
+// })
+// export class CoursesComponent {
+//   courses: any[] = [];
+
+//   constructor(private courseService: CourseServiceService,private router:Router) { }
+
+//   ngOnInit(): void {
+//     this.courseService.getCourses().subscribe((data: any[]) => {
+//       this.courses = data;
+//     }, error => {
+//       console.error('Error fetching courses', error);
+//     });
+//   }
+
+//   deleteCourse(course: any): void {
+//     this.courseService.deleteCourse(course.idCours).subscribe(response => {
+//       if (response === 'success') {
+//         this.courses = this.courses.filter(c => c.idCours !== course.idCours);
+//       } else {
+//         console.error('Failed to delete course');
+//       }
+//     });
+//   }
+//   goToComopnent(id:any){
+//     this.router.navigate(['course/',id])
+//   }
+//   goToUpdateCourse(id:any){
+//     this.router.navigate(['update-course/',id])
+//   }
+
+//   goToAddCourse(){
+//     this.router.navigate(['component/add-course'])
+//   }
+// }
+
+
+
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CourseServiceService } from 'src/Services/course-service.service';
 
@@ -7,10 +52,10 @@ import { CourseServiceService } from 'src/Services/course-service.service';
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
   courses: any[] = [];
 
-  constructor(private courseService: CourseServiceService,private router:Router) { }
+  constructor(private courseService: CourseServiceService, private router: Router) { }
 
   ngOnInit(): void {
     this.courseService.getCourses().subscribe((data: any[]) => {
@@ -27,16 +72,20 @@ export class CoursesComponent {
       } else {
         console.error('Failed to delete course');
       }
+    }, error => {
+      console.error('Error deleting course', error);
     });
   }
-  goToComopnent(id:any){
-    this.router.navigate(['course/',id])
-  }
-  goToUpdateCourse(id:any){
-    this.router.navigate(['update-course/',id])
+
+  goToComponent(id: any): void {
+    this.router.navigate(['course', id]);
   }
 
-  goToAddCourse(){
-    this.router.navigate(['component/add-course'])
+  goToUpdateCourse(id: any): void {
+    this.router.navigate(['update-course', id]);
+  }
+
+  goToAddCourse(): void {
+    this.router.navigate(['component/add-course']);
   }
 }
